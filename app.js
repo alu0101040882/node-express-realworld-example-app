3932e9e49c610e140a3142b9064a6289109e9af6
Daniel Suárez <alu0101040882@ull.edu.es>
2021-04-11 00:15:50 +0100

    fixes

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://192.168.32.2/conduit');
+  mongoose.connect(process.env.MONGODB_URI);
   mongoose.set('debug', true);
 }
 
